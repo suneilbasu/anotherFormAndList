46810e1752f235a50d999970b4ea428b2a493aa0
Suneil Basu <suneil@futureprogress.com>
2016-03-06 22:22:14 +0000

    working index with a form on the same page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,12 +3,14 @@ class ReportsController < ApplicationController
     @reports = Report.all
     @newReport = Report.new
   end
+
+
   def create
     @newReport = Report.new(report_params)
     if @newReport.save
-      redirect_to :action => 'root'
+      redirect_to :action => 'index'
     else
-      render(:action => 'new')
+      render(:action => 'index ')
     end
   end
   def report_params
